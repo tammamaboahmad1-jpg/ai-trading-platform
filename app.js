924ac9d66023445be6f7328044431abf10028871
tammamaboahmad1-jpg <tammamaboahmad1@gmail.com>
2026-05-20 04:59:22 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-function enterPlatform(){
+function enterPlatform() {
 
 document.getElementById("loginPage").style.display = "none";
 
 document.getElementById("appPage").style.display = "block";
 
-new TradingView.widget({
-"autosize": true,
-"symbol": "BINANCE:BTCUSDT",
-"interval": "15",
-"timezone": "Etc/UTC",
-"theme": "dark",
-"style": "1",
-"locale": "ar",
-"toolbar_bg": "#071733",
-"enable_publishing": false,
-"hide_top_toolbar": false,
-"hide_legend": false,
-"container_id": "tradingview"
-});
+alert("WELCOME TO T AI PLATFORM 🚀");
 
 }
